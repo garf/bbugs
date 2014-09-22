f221f09a41737c713a18bf0c6d97bca206bbbfac
Dinar Garipov <garipov.dinar@gmail.com>
2014-09-22 15:27:58 +0400

    Fixed project options

--- app/views/cabinet/projects/index.php
+++ app/views/cabinet/projects/index.php
@@ -4,42 +4,46 @@
 </div>
 <?php if(count($projects) != 0) { ?>
 
-<div>
-    <table class="table table-bordered">
-        <?php foreach ($projects as $project) { ?>
-            <tr>
-                <td style="border-left: 9px solid <?= ($project->creator == Auth::user()->id) ? '#007700' : '#dddd00' ?>">
-                    <h4><a href="<?= URL::route('issues-project', array($project->id)) ?>"><?= $project->title ?></a></h4>
+<div >
+    <?php foreach ($projects as $project) { ?>
+        <div class="panel panel-<?= ($project->creator == Auth::user()->id) ? 'success' : 'warning' ?>">
+            <div class="panel-heading"><h4><a href="<?= URL::route('issues-project', array($project->id)) ?>"><?= $project->title ?></a></h4></div>
+            <div class="panel-body">
+                <div class="col-md-10">
                     <div>
                         <span class="text-muted"><?= trans('projects.your_role') ?>:</span>
                         <?= trans('users.roles.' . $project->role) ?>
                     </div>
-                    <div><?= nl2br(Str::limit($project->description, 400)) ?></div>
+                    <div class="correct-wrap"><p><?= nl2br(Str::limit($project->description, 400)) ?></p></div>
                     <div class="text-right">
                         <span class="text-muted"><?= trans('projects.created_at') ?>: <?= date(trans('common.date_format'), $project->created) ?></span>
                     </div>
-                </td>
-                <td class="col-lg-2 text-center">
-                    <a href="<?= URL::route('project-info', array('project_id' => $project->id)) ?>" class="btn btn-sm btn-info btn-block ">
-                        <?= trans('projects.view_info') ?>
-                    </a>
-                    <?php if ($project->role == 'teamlead') { ?>
-                        <a href="<?= URL::route('edit-project', array('project_id' => $project->id)) ?>" class="btn btn-sm btn-warning btn-block ">
-                            <?= trans('projects.edit_project') ?>
-                        </a>
-                        <a href="<?= URL::route('add-to-project', array('project_id' => $project->id)) ?>" class="btn btn-sm btn-success btn-block ">
-                            <?= trans('projects.users') ?>
-                        </a>
-                        <a data-url="<?= URL::route('project-delete', array('id' => $project->id )) ?>"
-                           data-message="<?= trans('projects.delete_confirm', array('title' => $project->title)) ?>"
-                           class="btn btn-sm btn-danger btn-block delete-project">
-                            <?= trans('projects.delete_project') ?>
-                        </a>
-                    <?php } ?>
-                </td>
-            </tr>
-        <?php } ?>
-    </table>
+                </div>
+                <div class="col-md-2">
+                    <div class="well well-sm clearfix">
+                            <a href="<?= URL::route('project-info', array('project_id' => $project->id)) ?>" class="btn btn-xs btn-info btn-block">
+                                <?= trans('projects.view_info') ?>
+                            </a>
+                        <?php if ($project->role == 'teamlead') { ?>
+                                <a href="<?= URL::route('edit-project', array('project_id' => $project->id)) ?>" class="btn btn-xs btn-warning btn-block ">
+                                    <?= trans('projects.edit_project') ?>
+                                </a>
+                                <a href="<?= URL::route('add-to-project', array('project_id' => $project->id)) ?>" class="btn btn-xs btn-success btn-block ">
+                                    <?= trans('projects.users') ?>
+                                </a>
+                                <a data-url="<?= URL::route('project-delete', array('id' => $project->id )) ?>"
+                                   data-message="<?= trans('projects.delete_confirm', array('title' => $project->title)) ?>"
+                                   class="btn btn-xs btn-danger btn-block delete-project">
+                                    <?= trans('projects.delete_project') ?>
+                                </a>
+                        <?php } ?>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    <?php } ?>
 </div>
 
 <?php } else { ?>
