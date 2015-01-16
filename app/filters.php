a7686f7e762e340e8aee55f559b2c90b40954b74
root <root@kazned.ru>
2015-01-16 05:34:49 -0500

    removed HTTPS redirect

--- app/filters.php
+++ app/filters.php
@@ -13,10 +13,10 @@
 
 App::before(function($request)
 {
-    if( ! Request::secure())
-    {
-        return Redirect::secure(Request::path());
-    }
+//    if( ! Request::secure())
+//    {
+//        return Redirect::secure(Request::path());
+//    }
 });
 
 
