82090cea357e3880315bea7e1c0ddb0af7acea3a
Dinar Garipov <garipov.dinar@gmail.com>
2015-03-20 16:15:35 +0300

    ...

--- app/views/cabinet/issues/project.php
+++ app/views/cabinet/issues/project.php
@@ -113,6 +113,9 @@
             </tr>
         <?php } ?>
     </table>
+    <div class="text-right">
+        <?= $issues->links(); ?>
+    </div>
 <?php } else { ?>
     <div class="alert alert-info"><?= trans('issues.no_issues') ?></div>
 <?php } ?>
